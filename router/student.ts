848e9299e84b6605d7202756f88d3636b72a0a79
Malik Ammar Faisal <binfaisal.ammar@gmail.com>
2022-11-15 10:29:04 +0530

    get attendance for student

--- router/student.ts
+++ router/student.ts
@@ -15,8 +15,8 @@ const routes = async (app: FastifyInstance) => {
     );
 
     app.get("/api/get/student-notices/:id/:page", async (request, reply) => {
-        const { id, page } = request.params as { id: string, page: string };
-    
+        const { id, page } = request.params as { id: string; page: string };
+
         if (!id) {
             return reply.code(400).send({
                 error: "Bad Request",
@@ -29,7 +29,9 @@ const routes = async (app: FastifyInstance) => {
             return reply.status(400);
         }
 
-        const student = request.user.students.filter((student) => student.id === id);
+        const student = request.user.students.filter(
+            (student) => student.id === id
+        );
 
         if (student.length === 0) {
             return reply.code(401).send({
@@ -43,7 +45,7 @@ const routes = async (app: FastifyInstance) => {
             },
             select: {
                 class: true,
-            }
+            },
         });
 
         const notices = await prisma.notice.findMany({
@@ -56,7 +58,7 @@ const routes = async (app: FastifyInstance) => {
             skip: pagen * 10,
             orderBy: {
                 createdAt: "desc",
-            }
+            },
         });
 
         const count = await prisma.notice.count({
@@ -73,6 +75,56 @@ const routes = async (app: FastifyInstance) => {
             success: true,
             notices,
             page_count,
+        });
+    });
+
+    app.get("/api/get/student-attendance/:id/:date", async (request, reply) => {
+        const { id, date } = request.params as { id: string, date: string };
+
+        if (!id || !date) {
+            return reply.code(400).send({
+                error: "Bad Request",
+            });
+        }
+
+        const ddate = new Date(date);
+
+        if (isNaN(ddate.getTime())) {
+            return reply.code(400).send({
+                error: "Bad Request",
+            });
+        }
+
+        const student = request.user.students.filter(
+            (student) => student.id === id
+        );
+
+        if (student.length === 0) {
+            return reply.code(401).send({
+                error: "Unauthorized",
+            });
+        }
+
+        const attendance = await prisma.attendance.findMany({
+            where: {
+                studentId: id,
+                date: {
+                    gte: new Date(ddate.getFullYear(), ddate.getMonth(), ddate.getDate()),
+                    lt: new Date(ddate.getFullYear(), ddate.getMonth(), ddate.getDate() + 1),
+                }
+            },
+            select: {
+                date: true,
+                present: true,
+            },
+            orderBy: {
+                date: "desc",
+            },
+        });
+
+        reply.status(200).send({
+            success: true,
+            attendance,
         });
     });
 };
